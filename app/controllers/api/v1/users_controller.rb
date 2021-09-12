586fb02e2e0841f7c1ecc66ebcae937c9a4c13d7
utouto97 <utouto97@gmail.com>
2021-09-12 17:59:10 +0900

    add implementation of users_controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,12 +5,40 @@ module Api::V1
     before_action :authenticate_user!
 
     def index
+      render status: :ok, json: {
+        status: :ok,
+        user: current_user
+      }
     end
 
     def create
+      render status: :ok, json: {
+        status: :ok,
+        msg: "The user already exists.",
+      } and return if current_user
+
+      info = get_firebase_info
+      user = User.create({
+        firebase_uid: info[:uid],
+        email: nil,
+        **params.require(:user).permit(:username)
+      })
+
+      render status: :created, json: {
+        status: :created,
+        user: user
+      }
     end
 
     def update
+      current_user.update({
+        **params.require(:user).permit(:username)
+      })
+
+      render status: :created, json: {
+        status: :created,
+        user: current_user
+      }
     end
 
     def destroy
