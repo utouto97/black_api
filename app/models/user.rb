b0f41bf8edd035fea54fefdc118e9e6f86e2c984
utouto97 <utouto97@gmail.com>
2021-09-22 22:15:27 +0900

    remove relationships from User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,4 @@
 class User < ApplicationRecord
-  has_many :relationships
-  has_many :others, through: :relationships, source: :other
-
-  def follow(other_user)
-    unless self == other_user
-      self.relationships.find_or_create_by(other_id: other_user.id)
-    end
-  end
-
-  def unfollow(other_user)
-    relationship = self.relationships.find_by(other_id: other_user.id)
-    relationship.destroy if relationship
-  end
-
   has_many :room_users
   has_many :rooms, through: :room_users
 
